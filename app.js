e7764852109d29727652772dac6e237d8f19eeea
TaniaRivas1 <taniarivas140@gmail.com>
2022-03-17 16:42:20 -0300

    Servidor levantado

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+
+app.get('#', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'views', 'notfound.html'))
+})
+
+app.listen(port, () => console.log(`Servidor corriendo en puerto http://localhost:${port}`));
